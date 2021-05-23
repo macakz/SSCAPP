4c8605af400b5c2e180c5dd10457ebcdf0263c10
Alexander McKerrow <mckerrow4@gmail.com>
2021-05-23 19:43:22 +1200

    added more fields up to mem no

--- screen/StartScreen.jsx
+++ screen/StartScreen.jsx
@@ -1,10 +1,9 @@
 import React, { useState, useLayoutEffect } from 'react';
-import { KeyboardAvoidingView, SafeAreaView, StyleSheet, Text, TouchableOpacity, Image, TextInput, } from 'react-native';
+import { KeyboardAvoidingView, SafeAreaView, ScrollView, View, Button, StyleSheet, Text, TouchableOpacity, Image, TextInput, } from 'react-native';
 import { auth } from '../firebase'
 import { useSelector } from 'react-redux'
 import { useForm, Controller } from "react-hook-form";
 import { Picker } from '@react-native-picker/picker';
-import DateTimePicker from '@react-native-community/datetimepicker';
 
 const styles = StyleSheet.create({
     container: {
@@ -24,6 +23,15 @@ const styles = StyleSheet.create({
         padding: 10,
         width: 300,
 
+    },
+    userInputContainerLarge: {
+        borderWidth: 1,
+        borderRadius: 10,
+        borderColor: 'black',
+        padding: 10,
+        width: 300,
+        height: 200,
+        textAlignVertical: "top"
     },
     button: {
         alignSelf: 'center',
@@ -48,120 +56,256 @@ function StartScreen ({ navigation }) {
     const signOutUser = () => {
         auth().signOut()
     }
-    const [date, setDate] = useState(new Date())
-    return (
-        <KeyboardAvoidingView style={styles.container}>
-            <SafeAreaView>
-                <Text>National Health Index:</Text>
-                <Controller
-                    control={control}
-                    render={({ field: { onChange, onBlur, value } }) => (
-                        <TextInput
-                            keyboardType='numeric'
-                            placeholder="National Health Index"
-                            style={styles.userInputContainer}
-                            onBlur={onBlur}
-                            onChangeText={value => onChange(value)}
-                            value={value}
-                        />
-                    )}
-                    name="nationalHealthIndex"
-                    rules={{ required: true, maxLength: 10 }}
-                    defaultValue=""
-                />
-                {errors.nationalHealthIndex && <Text>This is required.</Text>}
-                <Controller
-                    control={control}
-                    render={({ field: { onChange, onBlur, value } }) => (
-                        <Picker
-                            selectedValue={value}
-                            onValueChange={onChange}
-                            onBlur={onBlur}
-                            value={value}
-                        >
-                            <Picker.Item label=" " value=" " />
-                            <Picker.Item label="Mr" value="Mr" />
-                            <Picker.Item label="Mrs" value="Mrs" />
-                            <Picker.Item label="Miss" value="Miss" />
-                            <Picker.Item label="Ms" value="Ms" />
-                        </Picker>
 
-                    )}
-                    name="title"
-                    rules={{ required: true, minLength: 2 }}
-                    defaultValue=""
-                />
-                {errors.title && <Text>This is required.</Text>}
-                <Controller
-                    control={control}
-                    render={({ field: { onChange, onBlur, value } }) => (
-                        <TextInput
-                            placeholder="First Name"
-                            style={styles.userInputContainer}
-                            onBlur={onBlur}
-                            onChangeText={value => onChange(value)}
-                            value={value}
-                        />
-                    )}
-                    name="firstName"
-                    rules={{ required: true }}
-                    defaultValue=""
-                />
-                {errors.firstName && <Text>This is required.</Text>}
-                <Controller
-                    control={control}
-                    render={({ field: { onChange, onBlur, value } }) => (
-                        <TextInput
-                            placeholder="Last Name"
-                            style={styles.userInputContainer}
-                            onBlur={onBlur}
-                            onChangeText={value => onChange(value)}
-                            value={value}
-                        />
-                    )}
-                    name="lastName"
-                    rules={{ required: true }}
-                    defaultValue=""
-                />
-                {errors.firstName && <Text>This is required.</Text>}
-                <Controller
-                    control={control}
-                    render={({ field: { onChange, onBlur, value } }) => (
-                        <TextInput
-                            placeholder="Preferred Name"
-                            style={styles.userInputContainer}
-                            onBlur={onBlur}
-                            onChangeText={value => onChange(value)}
-                            value={value}
-                        />
-                    )}
-                    name="preferredName"
-                    rules={{ required: true }}
-                    defaultValue=""
-                />
-                {errors.preferredName && <Text>This is required.</Text>}
-                <Controller
-                    control={control}
-                    render={({ field: { onChange, onBlur, value } }) => (
-                        <TextInput
-                            placeholder="Address"
-                            style={styles.userInputContainer}
-                            onBlur={onBlur}
-                            onChangeText={value => onChange(value)}
-                            value={value}
-                        />
-                    )}
-                    name="address"
-                    rules={{ required: true }}
-                    defaultValue=""
-                />
-                {errors.address && <Text>This is required.</Text>}
-                <TouchableOpacity style={styles.button} onPress={handleSubmit(onSubmit)}>
-                    
-                    <Text>Submit</Text>
-                </TouchableOpacity>
-            </SafeAreaView>
-        </KeyboardAvoidingView >
+
+    return (
+        <ScrollView>
+            <KeyboardAvoidingView behavior={'padding'} keyboardVerticalOffset={15} style={styles.container}>
+                <SafeAreaView>
+                    <Text>National Health Index:</Text>
+                    <Controller
+                        control={control}
+                        render={({ field: { onChange, onBlur, value } }) => (
+                            <TextInput
+                                keyboardType='numeric'
+                                placeholder="National Health Index"
+                                style={styles.userInputContainer}
+                                onBlur={onBlur}
+                                onChangeText={value => onChange(value)}
+                                value={value}
+                            />
+                        )}
+                        name="nationalHealthIndex"
+                        rules={{ required: true, maxLength: 10 }}
+                        defaultValue=""
+                    />
+                    {errors.nationalHealthIndex && <Text>This is required.</Text>}
+                    <Controller
+                        control={control}
+                        render={({ field: { onChange, onBlur, value } }) => (
+                            <Picker
+                                selectedValue={value}
+                                onValueChange={onChange}
+                                onBlur={onBlur}
+                                value={value}
+                            >
+                                <Picker.Item label=" " value=" " />
+                                <Picker.Item label="Mr" value="Mr" />
+                                <Picker.Item label="Mrs" value="Mrs" />
+                                <Picker.Item label="Miss" value="Miss" />
+                                <Picker.Item label="Ms" value="Ms" />
+                            </Picker>
+
+                        )}
+                        name="title"
+                        rules={{ required: true, minLength: 2 }}
+                        defaultValue=""
+                    />
+                    {errors.title && <Text>This is required.</Text>}
+                    <Controller
+                        control={control}
+                        render={({ field: { onChange, onBlur, value } }) => (
+                            <TextInput
+                                placeholder="First Name"
+                                style={styles.userInputContainer}
+                                onBlur={onBlur}
+                                onChangeText={value => onChange(value)}
+                                value={value}
+                            />
+                        )}
+                        name="firstName"
+                        rules={{ required: true }}
+                        defaultValue=""
+                    />
+                    {errors.firstName && <Text>This is required.</Text>}
+                    <Controller
+                        control={control}
+                        render={({ field: { onChange, onBlur, value } }) => (
+                            <TextInput
+                                placeholder="Last Name"
+                                style={styles.userInputContainer}
+                                onBlur={onBlur}
+                                onChangeText={value => onChange(value)}
+                                value={value}
+                            />
+                        )}
+                        name="lastName"
+                        rules={{ required: true }}
+                        defaultValue=""
+                    />
+                    {errors.firstName && <Text>This is required.</Text>}
+                    <Controller
+                        control={control}
+                        render={({ field: { onChange, onBlur, value } }) => (
+                            <TextInput
+                                placeholder="Preferred Name"
+                                style={styles.userInputContainer}
+                                onBlur={onBlur}
+                                onChangeText={value => onChange(value)}
+                                value={value}
+                            />
+                        )}
+                        name="preferredName"
+                        rules={{ required: true }}
+                        defaultValue=""
+                    />
+                    {errors.preferredName && <Text>This is required.</Text>}
+                    <Controller
+                        control={control}
+                        render={({ field: { onChange, onBlur, value } }) => (
+                            <TextInput
+                                placeholder="Address"
+                                style={styles.userInputContainer}
+                                onBlur={onBlur}
+                                onChangeText={value => onChange(value)}
+                                value={value}
+                            />
+                        )}
+                        name="address"
+                        rules={{ required: true }}
+                        defaultValue=""
+                    />
+                    {errors.address && <Text>This is required.</Text>}
+                    <Controller
+                        control={control}
+                        render={({ field: { onChange, onBlur, value } }) => (
+                            <TextInput
+                                keyboardType='numeric'
+                                placeholder="Phone Number"
+                                style={styles.userInputContainer}
+                                onBlur={onBlur}
+                                onChangeText={value => onChange(value)}
+                                value={value}
+                            />
+                        )}
+                        name="phone"
+                        rules={{ required: true }}
+                        defaultValue=""
+                    />
+                    {errors.phone && <Text>This is required.</Text>}
+                    <Controller
+                        control={control}
+                        render={({ field: { onChange, onBlur, value } }) => (
+                            <TextInput
+
+                                placeholder="E-mail"
+                                style={styles.userInputContainer}
+                                onBlur={onBlur}
+                                onChangeText={value => onChange(value)}
+                                value={value}
+                            />
+                        )}
+                        name="email"
+                        rules={{ required: true }}
+                        defaultValue=""
+                    />
+                    {errors.email && <Text>This is required.</Text>}
+                    <Controller
+                        control={control}
+                        render={({ field: { onChange, onBlur, value } }) => (
+                            <TextInput
+
+                                placeholder="GP Name"
+                                style={styles.userInputContainer}
+                                onBlur={onBlur}
+                                onChangeText={value => onChange(value)}
+                                value={value}
+                            />
+                        )}
+                        name="gpName"
+                        rules={{ required: true }}
+                        defaultValue=""
+                    />
+                    {errors.gpName && <Text>This is required.</Text>}
+                    <Controller
+                        control={control}
+                        render={({ field: { onChange, onBlur, value } }) => (
+                            <TextInput
+                                placeholder="GP Suburb"
+                                style={styles.userInputContainer}
+                                onBlur={onBlur}
+                                onChangeText={value => onChange(value)}
+                                value={value}
+                            />
+                        )}
+                        name="gpSuburb"
+                        rules={{ required: true }}
+                        defaultValue=""
+                    />
+                    {errors.gpSuburb && <Text>This is required.</Text>}
+                    <Controller
+                        control={control}
+                        render={({ field: { onChange, onBlur, value } }) => (
+                            <TextInput
+                                multiline={true}
+                                placeholder="Current Medications"
+                                style={styles.userInputContainerLarge}
+                                onBlur={onBlur}
+                                onChangeText={value => onChange(value)}
+                                value={value}
+                            />
+                        )}
+                        name="gpSuburb"
+                        rules={{ required: true }}
+                        defaultValue=""
+                    />
+                    {errors.currentMedication && <Text>This is required.</Text>}
+                    <Controller
+                        control={control}
+                        render={({ field: { onChange, onBlur, value } }) => (
+                            <TextInput
+                                multiline={true}
+                                placeholder="Drug Allergies"
+                                style={styles.userInputContainerLarge}
+                                onBlur={onBlur}
+                                onChangeText={value => onChange(value)}
+                                value={value}
+                            />
+                        )}
+                        name="Drug Allergies"
+                        rules={{ required: true }}
+                        defaultValue=""
+                    />
+                    {errors.drugAllergies && <Text>This is required.</Text>}
+                    <Controller
+                        control={control}
+                        render={({ field: { onChange, onBlur, value } }) => (
+                            <TextInput
+                                placeholder="Insurance Company"
+                                style={styles.userInputContainer}
+                                onBlur={onBlur}
+                                onChangeText={value => onChange(value)}
+                                value={value}
+                            />
+                        )}
+                        name="insuranceCompany"
+                        rules={{ required: true }}
+                        defaultValue=""
+                    />
+                    {errors.insuranceCompany && <Text>This is required.</Text>}
+                    <Controller
+                        control={control}
+                        render={({ field: { onChange, onBlur, value } }) => (
+                            <TextInput
+                                placeholder="Membership Number"
+                                style={styles.userInputContainer}
+                                onBlur={onBlur}
+                                onChangeText={value => onChange(value)}
+                                value={value}
+                            />
+                        )}
+                        name="membershipNumber"
+                        rules={{ required: true }}
+                        defaultValue=""
+                    />
+                    {errors.membershipNumber && <Text>This is required.</Text>}
+                    <TouchableOpacity style={styles.button} onPress={handleSubmit(onSubmit)}>
+                        <Text>Submit</Text>
+                    </TouchableOpacity>
+                </SafeAreaView>
+
+            </KeyboardAvoidingView >
+        </ScrollView>
     );
 }
 
