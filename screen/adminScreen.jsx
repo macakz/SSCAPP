0a407f1200c2c917fcaabde15fab038a926a8d6b
Alexander McKerrow <mckerrow4@gmail.com>
2021-06-21 17:01:59 +1200

    adminScreenStyle file added to seperate style from jsx

--- screen/adminScreen.jsx
+++ screen/adminScreen.jsx
@@ -1,26 +1,9 @@
 import React, {  useLayoutEffect } from 'react'
 import { SafeAreaView, StyleSheet, Text, TouchableOpacity, } from 'react-native'
 import { auth } from '../firebase'
+import styles from './adminScreenStyle.js';
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    button: {
-        alignSelf: 'center',
-        alignItems: 'center',
-        borderWidth: 1,
-        borderRadius: 10,
-        borderColor: '#eda488',
-        backgroundColor: '#eda488',
-        padding: 9,
-        margin: 4,
-        width: 180,
-    },
-})
+
 
 function AdminScreen ({ navigation }) {
     const signOut = () => {
---
+++ screen/adminScreenStyle.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    button: {
+        alignSelf: 'center',
+        alignItems: 'center',
+        borderWidth: 1,
+        borderRadius: 10,
+        borderColor: '#eda488',
+        backgroundColor: '#eda488',
+        padding: 9,
+        margin: 4,
+        width: 180,
+    },
+});
