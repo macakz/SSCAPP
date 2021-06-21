8ea10ac0b9d49e82d4b16ce77c52089291d9c5f8
Alexander McKerrow <mckerrow4@gmail.com>
2021-06-21 14:14:00 +1200

    loginScreenStyle file added to seperate style from jsx

--- screen/LoginScreen.jsx
+++ screen/LoginScreen.jsx
@@ -2,41 +2,7 @@ import React, { useState, useEffect, useLayoutEffect } from 'react';
 import { KeyboardAvoidingView, SafeAreaView, StyleSheet, Text, TouchableOpacity, Image, TextInput } from 'react-native';
 import { auth } from '../firebase';
 import { loginAdmin } from './accountHelper'
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    logo: {
-        alignSelf: 'center',
-        margin: 50,
-    },
-    inputContainer: {
-        borderWidth: 1,
-        borderRadius: 10,
-        borderColor: 'black',
-        padding: 10,
-        width: 300,
-
-    },
-    button: {
-        alignSelf: 'center',
-        alignItems: 'center',
-        borderWidth: 1,
-        borderRadius: 10,
-        borderColor: '#eda488',
-        backgroundColor: '#eda488',
-        padding: 9,
-        margin: 4,
-        width: 180,
-    },
-    buttonText: {
-
-    },
-});
+import styles from './loginScreenStyle.js';
 
 function LoginScreen ({ navigation }) {
     const [email, setEmail] = useState('')
---
+++ screen/loginScreenStyle.js
@@ -0,0 +1,36 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    logo: {
+        alignSelf: 'center',
+        margin: 50,
+    },
+    inputContainer: {
+        borderWidth: 1,
+        borderRadius: 10,
+        borderColor: 'black',
+        padding: 10,
+        width: 300,
+
+    },
+    button: {
+        alignSelf: 'center',
+        alignItems: 'center',
+        borderWidth: 1,
+        borderRadius: 10,
+        borderColor: '#eda488',
+        backgroundColor: '#eda488',
+        padding: 9,
+        margin: 4,
+        width: 180,
+    },
+    buttonText: {
+
+    },
+});
