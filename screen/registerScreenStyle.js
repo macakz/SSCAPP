108656ccadb6e2982794d813bec3257ea40a96ac
Alexander McKerrow <mckerrow4@gmail.com>
2021-06-25 10:52:05 +1200

    deleted comment of old code

--- screen/registerScreenStyle.js
+++ screen/registerScreenStyle.js
@@ -3,7 +3,6 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
     formContainer: {
         backgroundColor: '#fff',
-        // paddingLeft: 330,
         alignItems: 'center'
     },
     userInputContainer: {
